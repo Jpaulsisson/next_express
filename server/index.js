cb7db2a42dd6990686015c8b1f0de98c6a6c3c47
Paul Sisson <paulsissonsemail@gmail.com>
2024-01-15 12:00:41 -0600

    Add initial setup

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = express()
+    server.use(bodyParser.json())
+    server.use(bodyParser.urlencoded({ extended: true }))
+
+    server.get('/api/getRandom', (req, res) => {
+      const x = Math.floor(Math.random() * 1234567890)
+      const data = JSON.stringify(x)
+      return res.send(data)
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(3000, (err) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
+  })
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
