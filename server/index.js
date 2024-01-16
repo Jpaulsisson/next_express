46d8f6ae654abdcaf9dd490e30bccc34c622cfbd
Paul Sisson <paulsissonsemail@gmail.com>
2024-01-15 19:17:27 -0600

    I have learned what I created this to learn. Nice.

--- server/index.js
+++ server/index.js
@@ -6,17 +6,74 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const budgets = [
+  {
+    name: 'Groceries',
+    amount: 500,
+    remaining: 325,
+    daysUntilReset: 13,
+  },
+  {
+    name: 'Eating Out',
+    amount: 200,
+    remaining: 125,
+    daysUntilReset: 13,
+  },
+  {
+    name: 'Entertainment',
+    amount: 500,
+    remaining: 125,
+    daysUntilReset: 13,
+  },
+  {
+    name: 'Utilities',
+    amount: 400,
+    remaining: 0,
+    daysUntilReset: 13,
+  },
+  {
+    name: 'Mortgage',
+    amount: 1500,
+    remaining: 0,
+    daysUntilReset: 13,
+  },
+]
+
 app.prepare()
   .then(() => {
     const server = express()
     server.use(bodyParser.json())
     server.use(bodyParser.urlencoded({ extended: true }))
 
-    server.get('/api/getRandom', (req, res) => {
-      const x = Math.floor(Math.random() * 1234567890)
-      const data = JSON.stringify(x)
-      return res.send(data)
+    server.get('/api/getEnvelopes', (req, res) => {
+      res.send(budgets)
     })
+
+    server.get('/api/getEnvelopes/:id', (req, res) => {
+      const { id } = req.params;
+      const item = budgets.find(item => item.name === id)
+      item !== undefined ? res.send(item) : res.status(404).send();
+    })
+
+    server.put('/api/updateEnvelope', (req, res) => {
+      const { name, amount } = req.body;
+      const item = budgets.find(item => item.name === name);
+      const itemIdx = budgets.indexOf(item);
+      budgets[itemIdx].amount = amount;
+    })
+
+    server.post('/api/newEnvelope', (req, res) => {
+      const newItem = req.body;
+      newItem === undefined ? res.status(400).send() : budgets.push(newItem);
+    })
+
+    server.delete('/api/deleteEnvelope/:name', (req, res) => {
+      const { name } = req.params;
+      const item = budgets.find(item => item.name === name);
+      const itemIdx = budgets.indexOf(item);
+      budgets.splice(itemIdx, 1);
+    })
+
 
     server.get('*', (req, res) => {
       return handle(req, res)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,65 @@
 'use client';
 import axios from 'axios';
 import styles from './page.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+type Envelope = {
+  name: string;
+  amount: number;
+  remaining: number;
+  daysUntilReset: number;
+}
 
 export default function Home() {
 
-  const [random, setRandom] = useState(0);
+  const [envelopes, setEnvelopes] = useState<Envelope[] | null>(null);
 
-  // async function handleRandomChange() {
-  //   const new_random = await fetch('http://localhost:3000//api/getRandom', {
-  //     method: "GET",
-  //     headers: { 'Content-Type': 'application/json' }
-  //   })
-  //     .then(res => res.json())
-  //   setRandom(new_random);
-  // }
-  async function handleRandomChange() {
-    await axios.get('http://localhost:3000//api/getRandom').then((res) => res.status === 200 ? setRandom(res.data) : console.error("something went wrong"))
+  useEffect(() => {
+    async function handleGetEnvelopes() {
+      await axios.get('http://localhost:3000//api/getEnvelopes').then((res) => res.status === 200 ? setEnvelopes(res.data) : console.error("something went wrong"))
+    }
+    handleGetEnvelopes();
+  }, [])
+
+  async function handleGetEnvelopes() {
+    await axios.get('http://localhost:3000//api/getEnvelopes').then((res) => res.status === 200 ? setEnvelopes(res.data) : console.error("something went wrong"))
+  }
+
+  async function handleGetEnvelope(id: string) {
+    await axios.get(`http://localhost:3000//api/getEnvelopes/${id}`).then((res) => res.status === 200 ? console.log(res.data) : console.error("something went wrong"))
+  }
+
+  async function createNewEnvelope(envelope: Envelope) {
+    await axios.post(`/api/newEnvelope`, envelope).then(res => console.log(res))
+  }
+
+  async function updateEnvelope(name: string, amount: number) {
+    await axios.put(`/api/updateEnvelope`, { name, amount }).then(res => console.log(res))
+  }
+
+  async function deleteEnvelope(name: string) {
+    await axios.delete(`/api/deleteEnvelope/${name}`).then(res => console.log(res))
   }
 
   return (
     <main className={styles.mainContainer}>
-      <h2 className={styles.random}>Random is currently: {random} {random === 0 ? '(default)' : null}</h2>
-      <button onClick={handleRandomChange} className={styles.change}>Change Random</button>
+      {envelopes ?
+        envelopes.map((envelope) => {
+          return (
+            <div key={envelope.name}>
+              <h2>{envelope.name}</h2>
+              <p>${envelope.remaining} / {envelope.amount}</p>
+              <p>{envelope.daysUntilReset} days remaining</p>
+            </div>
+          )
+        })
+        :
+        null}
+      <button onClick={handleGetEnvelopes} className={styles.change}>Get Budgets</button>
+      <button onClick={() => handleGetEnvelope('Mortgage')} className={styles.change}>Get Budget</button>
+      <button onClick={() => createNewEnvelope({ name: "new", amount: 400, remaining: 100, daysUntilReset: 13 })} className={styles.change}>Add Budget</button>
+      <button onClick={() => updateEnvelope("Utilities", 800)} className={styles.change}>Change Budget</button>
+      <button onClick={() => deleteEnvelope("Utilities")} className={styles.change}>Delete Budget</button>
     </main>
   )
 }
